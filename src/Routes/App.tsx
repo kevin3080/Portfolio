d83d0559768d0e241ba2559ca4842a9f08565405
Kevin Pernia <kevinpernia29@gmail.com>
2023-10-25 17:03:56 -0500

    trabajando en responsive, vista projects y contacto1

--- src/Routes/App.tsx
+++ src/Routes/App.tsx
@@ -2,7 +2,7 @@ import { Abaut } from '../Components/Abaut';
 import { Contact } from '../Components/Contact';
 import { Education } from '../Components/Education';
 import { Inicio } from '../Components/Inicio';
-import { Navbar } from '../Components/Navbar';
+
 import { Projects } from '../Components/Projects';
 import { Footer } from '../Components/Footer';
 
