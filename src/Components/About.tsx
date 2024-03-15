65484f25ea3ac16c3e210c4c4173513a58dbd1b4
Kevin Pernia Chacon <37967904+kevin3080@users.noreply.github.com>
2024-03-14 22:24:44 -0500

    update2 About.tsx

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -35,6 +35,7 @@ export const About = () => {
               style="text-wrap"
               translationKey="about.px"
             />
+         </p>
 {/*
             <Translation translationKey="about.p" />
           </p>
